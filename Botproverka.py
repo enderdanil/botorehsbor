3aa60588d79d10147b6121f24c4d8793cd684b43
enderdanil <danil.sulaev@gmail.com>
2024-12-04 22:15:41 +0100

    Update Botproverka.py

--- Botproverka.py
+++ Botproverka.py
@@ -6,7 +6,7 @@ from telegram.error import NetworkError, BadRequest
 import asyncio
 
 # Установите ваш токен и ID чата
-TOKEN = "7573142030:AAF7ROHOll5b0rHd8QMQz6xwTeGWpd8-8F8"
+TOKEN = "7573142030:AAHKtW1Y6qGrnp7qSR6w_5O0DIDdiXX9l88"
 CHAT_ID = "-1002317588357"
 
 # Настроим логирование для отладки
